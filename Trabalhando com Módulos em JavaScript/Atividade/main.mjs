2a9eb917cb490cf24863c2ade71016297f6bedcf
Felippe <felippe_nvieira@hotmail.com>
2021-10-22 16:55:52 -0300

    Upload aula trabalhando com módulos

---
+++ Trabalhando com Módulos em JavaScript/Atividade/main.mjs
@@ -0,0 +1,5 @@
+import { mostraCidade, mostraIdade, mostraHobby } from "./funcoes.mjs";
+
+console.log(mostraIdade("Felippe", 26));
+console.log(mostraCidade("Felippe", "Ribeirão Preto"));
+console.log(mostraHobby("Felippe", "Ler livros"));
